53f19ce0974caf41530cb804f53bb864a83de7a3
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 11:35:39 -0600

    Parte del codigo es reciclado para un mejor desempeño y control de tiempo

--- Controllers/UserManagerController.cs
+++ Controllers/UserManagerController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnica_WebMaster.Areas.Identity.Data;
+using PruebaTecnica_WebMaster.Models.RoleManage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,34 @@ namespace PruebaTecnica_WebMaster.Controllers
 {
     public class UserManagerController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<PruebaTecnica_WebMasterUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserManagerController(UserManager<PruebaTecnica_WebMasterUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+            foreach (PruebaTecnica_WebMasterUser user in users)
+            {
+                var thisViewModel = new UserRolesViewModel();
+                thisViewModel.UserId = user.Id;
+                thisViewModel.Email = user.Email;
+                thisViewModel.FirstName = user.FirstName;
+                thisViewModel.LastName = user.LastName;
+                thisViewModel.Roles = await GetUserRoles(user);
+                userRolesViewModel.Add(thisViewModel);
+            }
+            return View(userRolesViewModel);
+        }
+
+        private async Task<List<string>> GetUserRoles(PruebaTecnica_WebMasterUser user)
+        {
+            return new List<string>(await _userManager.GetRolesAsync(user));
         }
     }
 }
