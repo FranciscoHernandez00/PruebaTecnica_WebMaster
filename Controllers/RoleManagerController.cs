bfce86164d9099604229a6bbdd0bf683756a7885
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 10:52:27 -0600

    Se añade un controlador para tener un control mas uniforme del modulo Roles

---
+++ Controllers/RoleManagerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica_WebMaster.Controllers
+{
+    public class RoleManagerController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleManagerController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+    }
+}
