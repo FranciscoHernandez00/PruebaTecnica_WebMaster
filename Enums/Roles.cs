4c194311ced61a7d5fe7aaa8bb5729d1cde41a66
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 10:14:17 -0600

    Roles predeterminados añadidos

---
+++ Enums/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica_WebMaster.Enums
+{
+    public enum Roles
+    {
+        Administrador,
+        Normal
+    }
+}
