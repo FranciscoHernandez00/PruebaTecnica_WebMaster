f4bb994febe84fa68553fe41ae064a6576eb8fcb
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 11:08:53 -0600

    La interfaz es capaz de mostrar al usuario los datos solicitados.

---
+++ Controllers/UserRolesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnica_WebMaster.Areas.Identity.Data;
+using PruebaTecnica_WebMaster.Models.RoleManage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica_WebMaster.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly UserManager<PruebaTecnica_WebMasterUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRolesController(UserManager<PruebaTecnica_WebMasterUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+            foreach (PruebaTecnica_WebMasterUser user in users)
+            {
+                var thisViewModel = new UserRolesViewModel();
+                thisViewModel.UserId = user.Id;
+                thisViewModel.Email = user.Email;
+                thisViewModel.FirstName = user.FirstName;
+                thisViewModel.LastName = user.LastName;
+                thisViewModel.Roles = await GetUserRoles(user);
+                userRolesViewModel.Add(thisViewModel);
+            }
+            return View(userRolesViewModel);
+        }
+        private async Task<List<string>> GetUserRoles(PruebaTecnica_WebMasterUser user)
+        {
+            return new List<string>(await _userManager.GetRolesAsync(user));
+        }
+    }
+}
