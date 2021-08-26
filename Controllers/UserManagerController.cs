85cd8c8eaf36f7eef06a14557f90fd439c23b38d
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 11:33:05 -0600

    Creacion de la interfez para un control de usuarios optimo y eficiente

---
+++ Controllers/UserManagerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica_WebMaster.Controllers
+{
+    public class UserManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
