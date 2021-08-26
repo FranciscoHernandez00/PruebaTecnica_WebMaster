93baf2d57fb80c7d36e94a8ed6a8d9bf2789f2b8
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 10:23:40 -0600

    Esta clase será reposible para "sembrar datos" predeterminados en la base de datos. En este caso, se agrega una función para generar los roles de usuario en la base de datos

---
+++ Areas/Identity/Data/ContextSeed.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica_WebMaster.Areas.Identity.Data
+{
+    public class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<PruebaTecnica_WebMasterUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Administrador.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Normal.ToString()));
+        }
+    }
+}
