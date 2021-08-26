de7eebc2514e21b29db264e9dd31f47faf8b4cc8
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 10:44:01 -0600

    Ahora que los roles prederminados estan listos para ser creados solo hace falata asignarle un rol por defecto a cada usuario nuevo que sea registrado

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -91,7 +91,7 @@ namespace PruebaTecnica_WebMaster.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    await _userManager.AddToRoleAsync(user, Enums.Roles.Normal.ToString());
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
