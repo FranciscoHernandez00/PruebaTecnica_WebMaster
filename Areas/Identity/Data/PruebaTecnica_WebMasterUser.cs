c2b1a5af2e257971afcd11e25189a3d18afc7f97
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 09:14:03 -0600

    Modulo Identity añadido

---
+++ Areas/Identity/Data/PruebaTecnica_WebMasterUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PruebaTecnica_WebMaster.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the PruebaTecnica_WebMasterUser class
+    public class PruebaTecnica_WebMasterUser : IdentityUser
+    {
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PruebaTecnica_WebMaster.Areas.Identity.Data;
+using PruebaTecnica_WebMaster.Data;
+
+[assembly: HostingStartup(typeof(PruebaTecnica_WebMaster.Areas.Identity.IdentityHostingStartup))]
+namespace PruebaTecnica_WebMaster.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PruebaTecnica_WebMasterDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PruebaTecnica_WebMasterDbContextConnection")));
+
+                services.AddDefaultIdentity<PruebaTecnica_WebMasterUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<PruebaTecnica_WebMasterDbContext>();
+            });
+        }
+    }
+}
