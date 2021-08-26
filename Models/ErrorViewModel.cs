e4beb8b2481b541995e60ca68fc7a9efddbe98ab
FranciscoHernandez00 <48956556+FranciscoHernandez00@users.noreply.github.com>
2021-08-26 09:08:57 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaTecnica_WebMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
